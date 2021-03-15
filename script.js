a930abb281b4e1b5ab8127d9957ad27ed7cd5124
Lesenok2807 <73076953+Lesenok2807@users.noreply.github.com>
2021-03-15 11:02:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 let body = document.body;
 let string = window.location.search;
 let url = 'https://api.github.com/users/Lesenok2807';
-let url = window.location.toString();
-let avatar_url = 'https://avatars.githubusercontent.com/u/73076953?v=4';
-let html_url = 'https://github.com/Lesenok2807';
 
 fetch(url)
-const getNameFromUrl = (url) => {
-  let getUrl = url.split('=');
-  let name = getUrl[1]; //
-  if(name == undefined) {
-  name = '';
-  }
-return name;
-}
-
-fetch(`https://api.github.com/users/${getNameFromUrl(url)}`)
     .then(res => res.json())
     .then(json => {
         console.log(json.avatar_url);
-        fetch(url)
+        console.log(json.name);
+        console.log(json.bio);
+        console.log(json.html_url);
+
+        let photo = new Image();
+        photo.src = json.avatar_url;
+        body.append(photo);
+
+        let name = document.createElement('p');
+        if (json.name != null) {
+            name.innerHTML = json.name;
+        } else {
             name.innerHTML = 'Информация о пользователе недоступна';
         }
         body.append(name);
         name.addEventListener("click", () => window.location = json.html_url);
-        name.addEventListener("click", () => window.location = 'https://webheroschool.github.io/badunka_35les/');
 
         let bio = document.createElement('p');
         if (json.bio != null) {
@@ -33,5 +30,6 @@ fetch(`https://api.github.com/users/${getNameFromUrl(url)}`)
             bio.innerHTML = 'Информация о пользователе недоступна';
         }
         body.append(bio);
+
     })
     .catch(err => alert('Информация о пользователе недоступна'));
