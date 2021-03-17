eff32105c6b96e0c3e3beffbe6db26c28db7896d
Олеся Иванова <ivanova.ov_87@mail.ru>
2021-03-17 07:56:32 +0300

    добавить изменения в promis

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-let body = document.body;
-let string = window.location.search;
-let url = 'https://api.github.com/users/Lesenok2807';
-let url = window.location.toString();
-let avatar_url = 'https://avatars.githubusercontent.com/u/73076953?v=4';
-let html_url = 'https://github.com/Lesenok2807';
+window.setTimeout(function () {
+  document.body.classList.add('loaded');
+  let body = document.body;
+  let string = window.location.search;
+  let url = 'https://api.github.com/users/Lesenok2807';
+  const date = new Date();
+  const getDate = new Promise((resolve, reject) => {
+    setTimeout(() => date ? resolve(date) : reject("Ошибка"), 100)
+  })
+  const getUrl = new Promise((resolve, reject) => {
+    setTimeout(() => url ? resolve(url) : reject("Ошибка URL!"), 100)
+  })
 
-fetch(url)
-const getNameFromUrl = (url) => {
-  let getUrl = url.split('=');
-  let name = getUrl[1]; //
-  if(name == undefined) {
-  name = '';
-  }
-return name;
-}
+   Promise.all([getUrl, getDate])
+      .then(([url, date]) => fetch(url))
+      .then(res => res.json())
+      .then(json => {
+          console.log(json.avatar_url);
+          console.log(json.name);
+          console.log(json.bio);
+          console.log(json.html_url);
 
-fetch(`https://api.github.com/users/${getNameFromUrl(url)}`)
-    .then(res => res.json())
-    .then(json => {
-        console.log(json.avatar_url);
-        fetch(url)
-            name.innerHTML = 'Информация о пользователе недоступна';
-        }
-        body.append(name);
-        name.addEventListener("click", () => window.location = json.html_url);
-        name.addEventListener("click", () => window.location = 'https://webheroschool.github.io/badunka_35les/');
+          let photo = new Image();
+          photo.src = json.avatar_url;
+          body.append(photo);
 
-        let bio = document.createElement('p');
-        if (json.bio != null) {
-            bio.innerHTML = json.bio;
-        } else {
-            bio.innerHTML = 'Информация о пользователе недоступна';
-        }
-        body.append(bio);
+          let name = document.createElement('p');
+          if (json.name != null) {
+              name.innerHTML = json.name;
+          } else {
+              name.innerHTML = 'Информация о пользователе недоступна';
+          }
+          body.append(name);
+          name.addEventListener("click", () => window.location = json.html_url);
+
+          let bio = document.createElement('p');
+          if (json.bio != null) {
+              bio.innerHTML = json.bio;
+          } else {
+              bio.innerHTML = 'Информация о пользователе недоступна';
+          }
+          body.append(bio);
+          body.append(date)
+
     })
-    .catch(err => alert('Информация о пользователе недоступна'));
+
+    .catch(err => console.log('Информация о пользователе недоступна'));
+ }, 3000);
